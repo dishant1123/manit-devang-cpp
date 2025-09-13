3fef9b6a17bc923710049e8f9abcbb99978d25ef
dishant1123 <dishantshah1417@gmail.com>
2025-09-13 16:55:16 +0530

    ...

--- 13sept_harshil.cpp
+++ 13sept_harshil.cpp
@@ -7,6 +7,7 @@
 2. method  over riding  ==> run time  
 */
 
+/*
 #include<iostream>
 using namespace std;
 class animal 
@@ -47,8 +48,88 @@ int main()
     return 0;     
 
 }
-
+*/
 /*
 int a =10 
 int *p =&a   // pointer declare inti   
-*/
+*/ 
+
+#include<iostream>
+using namespace std;
+class bank 
+{
+    protected :
+        string name;
+        int balance;
+    public : 
+        bank(string name , int balance)
+        {
+            this->name=name;
+            this->balance=balance;
+        }
+    virtual void display()
+    {
+        cout<<"name : "<<name<<"\n";
+        cout<<"balance : "<<balance<<"\n";
+    }
+    virtual void interest()
+    {
+        cout<<"general bank so no interest.\n";
+    }
+};
+class saving_ac :public bank 
+{
+    public : 
+        double interest_rate;
+    
+    saving_ac(string name ,int balance , double interest_rate) : bank(name , balance)
+    {
+        this->interest_rate=interest_rate;
+    }
+    void display() override
+    {
+        cout<<"saving ac information\n"; 
+        bank::display();
+        cout<<"interest rate : "<<interest_rate<<"\n";
+    }
+    void interest() override
+    {
+        double int_amt = balance * (interest_rate /100);
+        cout<<"interest amount : "<<int_amt<<"\n";
+    }
+};
+class current_ac : public bank 
+{
+    public :
+        double overdraft_limit; 
+    current_ac(string name, int balance ,double overdraft_limit) : bank(name , balance)
+    {
+        this->overdraft_limit=overdraft_limit;
+    }
+    void display() override
+    {
+        cout<<"current ac information\n";
+        bank::display();
+        cout<<"overdraft limit : "<<overdraft_limit<<"\n";
+    }
+    void interest() override
+    {
+        cout<<"current ac so no interest.\n";
+    }
+};
+int main()
+{
+    bank *b; 
+   
+    saving_ac s1("jay",25000,10); 
+    b =&s1; 
+    b->display();
+    b->interest();
+
+    current_ac c1("harshil",35000,1000);
+    b=&c1;
+    b->display();
+    b->interest();
+
+    return 0; 
+}
