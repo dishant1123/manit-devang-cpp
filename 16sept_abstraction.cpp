e8c6c208f40a0f9e40e38ce222680cb7d7bab74c
dishant1123 <dishantshah1417@gmail.com>
2025-09-16 17:48:17 +0530

    ...

--- 16sept_abstraction.cpp
+++ 16sept_abstraction.cpp
@@ -44,6 +44,7 @@ int main()
 */
 // 2 .interfaces (pure abstract class ) :
 
+/*
 #include<iostream>
 using namespace std;
 class payment 
@@ -86,4 +87,69 @@ int main()
     p2->pay(9000);
     p2->refund(200);
     return 0;
+}
+*/
+
+// private , protected in abstraction  : 
+
+#include<iostream>
+using namespace std;
+class employees 
+{
+    private : 
+        string name; 
+    protected : 
+        int  salary ; 
+    public : 
+        employees(string name,  int salary)
+        {
+            this->name=name;
+            this->salary=salary;
+        }
+    virtual void showdetails()=0 ; 
+
+    string getname()  // get method  ==> encap 
+    {
+        return name; 
+    }
+};
+class manager : public employees 
+{
+    public :
+        string m_name;  
+        manager(string name, int salary,string m_name) : employees(name,salary)
+        {
+            this->m_name=m_name;
+        }
+    void showdetails() override
+    {
+        cout<<"manager name : "<<m_name<<"\n";
+        cout<<"employee name : "<<getname()<<"\n";
+        cout<<"employee salary : "<<salary<<"\n";
+    }
+};
+class developer : public employees 
+{
+    public : 
+        string d_name; 
+    developer(string name, int salary,string d_name) : employees(name,salary)
+    {
+        this->d_name=d_name;
+    }
+    void showdetails() override
+    {
+        cout<<"developer name : "<<d_name<<"\n";
+        cout<<"employee name : "<<getname()<<"\n";
+        cout<<"employee salary : "<<salary<<"\n";
+    }
+};
+int main()
+{
+    employees *e1= new manager("harshil",10000,"prince");
+    employees *e2=new developer("viral",40000,"jay");
+
+    e1->showdetails(); 
+
+    e2->showdetails();
+    return 0; 
 }
