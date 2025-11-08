8fce1aa31fe3c4cd327d6a08a82fd0b1329602cd
dishant1123 <dishantshah1417@gmail.com>
2025-11-08 12:01:01 +0530

    ....

--- 8nov_harshil_naitra.cpp
+++ 8nov_harshil_naitra.cpp
@@ -3,7 +3,7 @@ abstration  : it is  the  process  of  hiding  the  details  of  an  object  and
 
 pure virtual function  : 
 */
-
+/*
 #include<iostream>
 using namespace std; 
 class shape
@@ -55,4 +55,96 @@ int main()
     s->area();
 
     return 0; 
+}
+*/
+
+// bank  : 
+
+#include<iostream>
+using namespace std;
+class bank 
+{
+    public : 
+        virtual void  deposit(int amt)=0;
+        virtual void  withdraw(int amt)=0;
+        virtual void  displaybalance()=0;
+};
+
+class savings : public bank 
+{
+    private : 
+        int balance; 
+    public : 
+        savings(int balance)
+        {
+            this->balance=balance;
+        }
+        void deposit(int amt) override
+        {
+            balance=balance+amt;
+            cout<<"deposited amount : "<<amt<<endl;
+        }
+        void withdraw(int amt) override
+        {
+            balance=balance-amt;
+            cout<<"withdrawed amount : "<<amt<<endl;
+        }
+        void displaybalance() override
+        {
+            cout<<"balance : "<<balance<<endl;
+        }
+};
+class current : public bank 
+{
+    private : 
+        int balance; 
+    public :
+        current(int balance)
+        {
+            this->balance=balance;
+        }
+        void deposit(int amt) override
+        {
+            balance=balance+amt;
+            cout<<"deposited amount : "<<amt<<endl;
+        }
+        void withdraw(int amt) override
+        {
+            if (balance -amt >10000) 
+            {
+                balance=balance-amt;
+                cout<<"withdrawed amount : "<<amt<<endl;
+            }
+            else 
+            {
+                cout<<"min balance is 10000"<<endl;
+            }
+        }
+        void displaybalance() override
+        {
+            cout<<"balance : "<<balance<<endl;
+        }
+};
+
+int main()
+{
+    bank *b; 
+
+    savings s1(10000);
+    current c1(20000); 
+
+    cout<<"savings account "<<endl;
+    b=&s1; 
+    b->deposit(1000);
+    b->withdraw(500);
+    b->displaybalance();
+
+    cout<<endl;
+    cout<<"current account "<<endl;
+    b=&c1;
+    b->deposit(1000);
+    b->withdraw(500);
+    b->displaybalance();
+     return 0; 
+
 }
