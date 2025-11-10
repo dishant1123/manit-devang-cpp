77b153806cf7183849f338b941c89b2e924c41cd
dishant1123 <dishantshah1417@gmail.com>
2025-11-10 16:38:06 +0530

    ....

--- 10nov_pratham.cpp
+++ 10nov_pratham.cpp
@@ -4,6 +4,7 @@ friend  funcion  : it is  not member  of call but access private  and  protected
 declare ==> with in  class  
 friend  ==> keyword 
 */
+/*
 #include <iostream>
 using namespace std;
 class box 
@@ -27,4 +28,42 @@ int main()
     box b(10); 
     display(b);
     return 0; 
+}
+*/
+
+// protected class in friend  function  : 
+
+#include <iostream>
+using namespace std;
+class student 
+{
+    protected :
+        string name ;
+        int marks; 
+
+    public :
+        student(string n, int m)
+        {
+            name = n;
+            marks = m;
+        }
+    friend void display(student s);
+};
+class person : public student 
+{
+    public :
+        person(string n, int a) : student(n, a)
+        {
+        }
+
+};
+void display(student s)
+{
+    cout<<"name is "<<s.name<<" and marks is "<<s.marks<<endl;
+}
+int main()
+{
+    student s("pratham", 100);
+    display(s);
+    return 0;
 }
